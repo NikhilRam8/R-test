4af78e1da22a313ba03961e57de09b4aad9c8591
Nikhi <126812727+NikhilRam8@users.noreply.github.com>
2025-02-25 23:00:57 -0500

    Success

---
+++ test.R
@@ -0,0 +1,13 @@
+# Exploring Markdown Syntax and R Functions
+
+# 1. Table Creation Using Markdown
+# | Column 1      | Column 2      |
+# | ------------- | ------------- |
+# | Row 1, Cell 1 | Row 1, Cell 2 |
+# | Row 2, Cell 1 | Row 2, Cell 2 |
+
+# 2. Attaching a Hyperlink
+# [Visit our website](http://www.examplewebsite.com)
+
+# 3. Inserting an Image
+# ![Alternative text describing the image](http://www.exampleimage.com/image.jpg)
